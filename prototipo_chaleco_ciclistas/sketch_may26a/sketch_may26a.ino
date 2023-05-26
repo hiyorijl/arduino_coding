758a6d87f5bdf6b4a6648a19993467d6912669ab
hiyori <kaekazeh@gmail.com>
2023-05-26 16:49:09 -0500

    CODE WORKS. performant code + comments to remember the learning (BEFORE and AFTER) fixed: (Don't Repeat Yourself (DRY))

--- prototipo_chaleco_ciclistas/sketch_may26a/sketch_may26a.ino
+++ prototipo_chaleco_ciclistas/sketch_may26a/sketch_may26a.ino
@@ -6,41 +6,31 @@ int leds[]= {4,5,6,7,8,9,10,11,12,13};
 int tiempoRet=400;
 
 void setup(){
-  pinMode(leds[0], OUTPUT);
-  pinMode(leds[1], OUTPUT);
-  pinMode(leds[2], OUTPUT);
-  pinMode(leds[3], OUTPUT);
-  pinMode(leds[4], OUTPUT);
-  pinMode(leds[5], OUTPUT);
-  pinMode(leds[6], OUTPUT);
-  pinMode(leds[7], OUTPUT);
-  pinMode(leds[8], OUTPUT);
-  pinMode(leds[9], OUTPUT);
+  for (int i = 0; i < 10; i++){
+    pinMode (leds[i], OUTPUT);
+  }
+ // BEFORE optimization
+ // pinMode(leds[0], OUTPUT);
+ // pinMode(leds[1], OUTPUT);
+ // pinMode(leds[2], OUTPUT);
+ // pinMode(leds[3], OUTPUT);
+ // pinMode(leds[4], OUTPUT);
+ // pinMode(leds[5], OUTPUT);
+ // pinMode(leds[6], OUTPUT);
+ // pinMode(leds[7], OUTPUT);
+ // pinMode(leds[8], OUTPUT);
+ // pinMode(leds[9], OUTPUT);
 }
 
 void loop(){
-  digitalWrite(leds[0], HIGH);
-  digitalWrite(leds[1], HIGH);
-  digitalWrite(leds[2], HIGH);
-  digitalWrite(leds[3], HIGH);
-  digitalWrite(leds[4], HIGH);
-  digitalWrite(leds[5], HIGH);
-  digitalWrite(leds[6], HIGH);
-  digitalWrite(leds[7], HIGH);
-  digitalWrite(leds[8], HIGH);
-  digitalWrite(leds[9], HIGH);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(leds[i], HIGH);
+  }
   delay(tiempoRet);
 
-  digitalWrite(leds[0], LOW);
-  digitalWrite(leds[1], LOW);
-  digitalWrite(leds[2], LOW);
-  digitalWrite(leds[3], LOW);
-  digitalWrite(leds[4], LOW);
-  digitalWrite(leds[5], LOW);
-  digitalWrite(leds[6], LOW);
-  digitalWrite(leds[7], LOW);
-  digitalWrite(leds[8], LOW);
-  digitalWrite(leds[9], LOW);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(leds[i], LOW);
+  }
   delay(tiempoRet);
 }
 
