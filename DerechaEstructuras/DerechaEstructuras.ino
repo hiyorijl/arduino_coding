b0301ccd339db263e1eb367dc64cdaba500a4c24
hiyori <kaekazeh@gmail.com>
2023-05-30 04:22:07 -0500

    LEDs_flecha_derecha_simple

---
+++ DerechaEstructuras/DerechaEstructuras.ino
@@ -0,0 +1,6 @@
+// Programa derecha_flecha_LEDs_estructura encendido de LEDs
+// Realiza el encendido de 10 LEDS hacia la derecha (forma de flecha)
+
+// Variables
+int bot = 2;
+int led[] = {4,5,6,7,8,9,10,11,12,13};
